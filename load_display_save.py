0b7230b49bf7ec56045cbbc34e09469470fcd524
charles <chargree@gmail.com>
2015-01-17 17:22:51 -0500

    Drawing of primitives (i.e, Lines, Rectangles and Circles)

--- load_display_save.py
+++ load_display_save.py
@@ -1,24 +1,47 @@
-import argparse
+import numpy as np
 import cv2
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i","--image", required = True, help = "Path to the image")
-args = vars(ap.parse_args())
-
-image = cv2.imread(args["image"])
-cv2.imshow("Original", image)
-(b, g, r) = image[0,0]
-print "Pixels at (0,0) - Red %d, Green %d, Blue: %d" % (r, g, b)
-
-image[0,0] = (0, 0, 255)
-(b, g, r) = image[0,0]
-print "Pixels at (0,0) - Red %d, Greene %d, Blue: %d" % (r, g, b)
-
-corner = image[0:100, 0:100]
-cv2.imshow("Corner", corner)
-
-image[0:100, 0:100] = (0, 255, 0)
-
-cv2.imshow("Updated", image)
-
+canvas = np.zeros((300,300,3), dtype = "uint8")
+green = (0, 255, 0)
+cv2.line(canvas, (0,0), (300,300), green)
+cv2.imshow("Canvas", canvas)
 cv2.waitKey(0)
+
+red = (0,0,255)
+cv2.line(canvas,(300,0),(0,300), red,3)
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
+
+cv2.rectangle(canvas,(10,10),(60,80),green)
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
+
+cv2.rectangle(canvas,(50,200),(200,225),red,5)
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
+
+blue = (255,0,0)
+cv2.rectangle(canvas,(200,50),(225,125), blue,-1)
+cv2.imshow("Canvas",canvas)
+cv2.waitKey(0)
+
+canvas = np.zeros((300,300,3),dtype = "uint8")
+(centerX, centerY) = (canvas.shape[1] /2, canvas.shape[0] /2)
+white = (255, 255,255)
+
+for r in xrange(0, 175, 25) :
+    cv2.circle(canvas, (centerX,centerY),r,white)
+
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
+
+for i in xrange(0,25) :
+    radius = np.random.randint(5, high = 200)
+    color = np.random.randint(0, high = 256, size = (3,))
+    pt = np.random.randint(0, high = 300, size = (2,))
+
+    cv2.circle(canvas, tuple(pt), radius, tuple(color), -1)
+
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
+
