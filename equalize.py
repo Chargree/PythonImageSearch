0cc34de200f0339722602507b79a525d1718c287
charles <chargree@gmail.com>
2015-02-07 16:28:47 -0500

    Equalize

--- equalize.py
+++ equalize.py
@@ -10,5 +10,6 @@ image = cv2.imread(args["image"])
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 eq = cv2.equalizeHist(image)
-cv2.imshow("Histogram Equalization", np.hstack([image, eq]))
+cv2.imshow("Histogram Equalization", np.vstack([image, eq]))
 cv2.waitKey(0)
+
